c5f4f3a8ff7065c358331bbb825709f913b92a15
Salem <salem.b.mabrouk@gmail.com>
2017-01-20 17:57:28 +0100

    First commit

---
+++ program.js
@@ -0,0 +1,20 @@
+var MaisonJSON = [{
+	"title": "Belle Maison au bord de la mer",
+  "type": "Maison"
+	"lieu": "Hajeb",
+  "codePostal":99999,
+  "prix":100000,
+  "nombrePieces": 6,
+  "commentaire": "Maison avec piscine "
+}];
+
+var EstimationJSON = [{
+  "type":"maison",
+	"lieu": "Hajeb",
+  "codePostal":99999,
+  "prix": {
+			"prixMin": 30000,
+			"prixMax": 300000,
+			"prixMoyen": 150000
+		}
+}];
